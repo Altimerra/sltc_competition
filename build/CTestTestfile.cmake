332001db06e4c22ad3d10e9538ac238405494981
Harindu Bandara <harindu@quasar>
2023-04-22 11:48:56 +0530

    branch init

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dulanjana/sltc_2023/src
-# Build directory: /home/dulanjana/sltc_2023/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("sltc_robots")
--- build/sltc_robots/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dulanjana/sltc_2023/src/sltc_robots
-# Build directory: /home/dulanjana/sltc_2023/build/sltc_robots
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
